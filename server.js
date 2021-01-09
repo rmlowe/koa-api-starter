8a961021825885994f46cfc79b4545118b993e73
Robert Lowe <robertmlowe@rmlowe.com>
2021-01-09 18:09:51 +0800

    Creating Company Model

--- server.js
+++ server.js
@@ -2,8 +2,14 @@ const Koa = require('koa');
 const Router = require('koa-router');
 const bodyParser = require('koa-parser');
 
+
 const app = new Koa();
 const PORT = 4000;
+
+const db = require('./models');
+db.sequelize.sync()
+    .then(() => console.log('models synced!'))
+    .catch(er => console.log(err));
 
 app.listen(PORT);
 console.log(`Server is listening on PORT ${PORT}`);
