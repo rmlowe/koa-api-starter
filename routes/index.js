4d99a76724dadf38becbd083d3c69dbdb22047a9
Robert Lowe <robertmlowe@rmlowe.com>
2021-01-09 18:22:47 +0800

    Adding Controllers And Routes

---
+++ routes/index.js
@@ -0,0 +1,6 @@
+const Router = require('koa-router');
+const router = new Router();
+
+//define all your routes
+
+ module.exports = router;
--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 const Koa = require('koa');
 const Router = require('koa-router');
 const bodyParser = require('koa-parser');
+const _ = require('lodash');
 
+const router = require('./routes');
 
 const app = new Koa();
 const PORT = 4000;
 
 const db = require('./models');
-db.sequelize.sync()
-    .then(() => console.log('models synced!'))
-    .catch(er => console.log(err));
+  db.sequelize.sync()
+      .then(() => console.log('models synced!'))
+      .catch((err) => console.log(err));
+
+app.use(bodyParser());
+app.use(router.routes());
 
 app.listen(PORT);
 console.log(`Server is listening on PORT ${PORT}`);
